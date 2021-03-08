4deb770518ae08cef9cd324b6d76c53fcc1cd331
cedarwarman <cedardalewarman@gmail.com>
2021-03-08 20:49:39 +0000

    Add C to F conversion

--- python/read_sensor_data.py
+++ python/read_sensor_data.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Data reading adapted from https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
+
 import Adafruit_DHT
 
 # Defining some constants
@@ -8,10 +10,14 @@ dht_pin = 4
 
 # Reading the sensor and printing results
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+    humidity, temp_c = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f} C  Humidity={1:0.1f}%".format(temperature, humidity))
+    if humidity is not None and temp_c is not None:
+        # Adding C to F conversion
+        temp_f = (temp_c * 9/5) + 32
+
+        print("Temp={0:0.1f} C ({1:0.1f} F) Humidity={2:0.1f}%".format(temp_c, temp_f, humidity))
+
     else:
         print("Failed to retrieve data from humidity sensor")
 
