47fe57121c00295b43c13dab7aaf14457e0622de
cedarwarman <cedardalewarman@gmail.com>
2021-03-10 05:45:46 +0000

    Add file output

--- python/read_sensor_data.py
+++ python/read_sensor_data.py
@@ -2,11 +2,21 @@
 
 # Data reading adapted from https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
 
+import os
+import time
 import Adafruit_DHT
 
 # Defining some constants
 dht_sensor = Adafruit_DHT.DHT22
 dht_pin = 4
+
+# Opening the file to write out
+try:
+    f = open('/home/pi/Documents/pi_sensor/output/sensor_output.csv', 'a+')
+    if os.stat('/home/pi/Documents/pi_sensor/output/sensor_output.csv').st_size == 0:
+            f.write('date\ttime\ttemp_c\ttemp_f\thumidity\r\n')
+except:
+    pass
 
 # Reading the sensor and printing results
 while True:
@@ -18,6 +28,12 @@ while True:
 
         print("Temp={0:0.1f} C ({1:0.1f} F) Humidity={2:0.1f}%".format(temp_c, temp_f, humidity))
 
+        # Writing to the file
+        f.write('{0}\t{1}\t{2:0.1f}\t{3:0.1f}\t{4:0.1f}\r\n'.format(time.strftime('%m/%d/%y'),
+        time.strftime('%H:%M'), temp_c, temp_f, humidity))
+
     else:
         print("Failed to retrieve data from humidity sensor")
+
+    time.sleep(120)
 
