d8150e8cb1070c4ef7f62127d1debc7bab7be7e4
cedarwarman <cedardalewarman@gmail.com>
2021-05-28 22:38:04 -0700

    Fix time bug

--- python/read_sensor_data.py
+++ python/read_sensor_data.py
@@ -22,7 +22,7 @@ def open_output_file():
         pass
 
 ### Read the sensor 
-def read_sensor(dht_sensor, dht_pin):
+def read_sensor(dht_sensor, dht_pin, input_time):
     humidity, temp_c = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
 
     if humidity is not None and temp_c is not None:
@@ -32,23 +32,24 @@ def read_sensor(dht_sensor, dht_pin):
         # Test print
         # print("Temp={0:0.1f} C ({1:0.1f} F) Humidity={2:0.1f}%".format(temp_c, temp_f, humidity))
 
-        sensor_list = [time.strftime('%H:%M:%S'), temp_c, temp_f, humidity]
+        sensor_list = [time.strftime('%H:%M:%S', input_time), temp_c, temp_f, humidity]
         return(sensor_list)
 
     else:
         print("Failed to retrieve data from humidity sensor")
 
 ### Append to file
-def append_file(input_file_handle, input_list):
+def append_file(input_file_handle, input_list, input_time):
     try:
-        input_file_handle.write('{0}\t{1}\t{2:0.1f}\t{3:0.1f}\t{4:0.1f}\r\n'.format(time.strftime('%Y-%m-%d'),
+        input_file_handle.write('{0}\t{1}\t{2:0.1f}\t{3:0.1f}\t{4:0.1f}\r\n'.format(time.strftime('%Y-%m-%d',
+        input_time),
         input_list[0], input_list[1], input_list[2], input_list[3]))
         input_file_handle.flush()
     except:
         print("Fail to write to file")
 
 ### Append to Google sheet
-def append_google_sheet(input_list, sheet_key):
+def append_google_sheet(input_list, sheet_key, input_time):
     try:
         # Setting up the service account info
         # (/home/pi/.config/gspread/service_account.json)
@@ -58,7 +59,7 @@ def append_google_sheet(input_list, sheet_key):
         sheet = gc.open_by_key(sheet_key).sheet1    
 
         # Writing the data
-        append_list = [time.strftime('%Y-%m-%d'), 
+        append_list = [time.strftime('%Y-%m-%d', input_time), 
         input_list[0], 
         round(input_list[1], 1), 
         round(input_list[2], 1), 
@@ -69,23 +70,32 @@ def append_google_sheet(input_list, sheet_key):
 
 
 def main():
+    print("started program")
     # Defining some constants
     dht_sensor = Adafruit_DHT.DHT22
     dht_pin = 4
     start_time = time.time() # Initial time for fancy sleep
 
+
     while True:
+        # Gives everything the same time to fix a bug that came from calling 
+        # time() a bunch of times
+        read_time = time.localtime()
+        
         f = open_output_file()
-        sensor_output = read_sensor(dht_sensor, dht_pin)
+        sensor_output = read_sensor(dht_sensor, dht_pin, read_time)
         # print(sensor_output)
-        append_file(f, sensor_output)
+        append_file(f, sensor_output, read_time)
         # Appends to sheet that has all the data
-        append_google_sheet(sensor_output, '1v0W5jSeF_wNWV9JCeZlG_zNOOsADDlPmwQSymcgLEJQ')
+        append_google_sheet(sensor_output,
+        '1Byxkjq1Jl_O6cyOrBp6yO_vHAI4FSbJkKZetLsOhwZc', read_time)
         # Appends to sheet that just has the past 7 days (pruned by another
         # script on a different raspberry pi)
-        append_google_sheet(sensor_output, '1pb0uU-8VST4gp8zkbDiO0YNfAKdsnvoKgLZ63juG27I')
+        append_google_sheet(sensor_output,
+        '1cF2DWhlWZ6CYsZU7B6akx4X9EFrqh33oHst0ZKH6AYA', read_time)
         # Appends to sheet that has the past 30 days
-        append_google_sheet(sensor_output, '19EM5mkx01ewRcHHf3mJaULZlADGzxKCkPbC00iH8mNM')
+        append_google_sheet(sensor_output,
+        '150Or_GGUti5W4-Y0E3EUxnqdM7RJi82mYjM9qIvii3k', read_time)
         time.sleep(60.0 - ((time.time() - start_time) % 60.0))
 
 
